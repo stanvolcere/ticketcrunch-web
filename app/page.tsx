10735572b3093dfda9b24ef5e7ddcf934f6e2029
Stan Volcere <stanvolcere@gmail.com>
2024-10-08 23:28:59 +0100

    CHORE - changed to yarn + added new font - figtree

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import React from "react";
 
 export default function Home() {
   return (
-    <div>TicketCrunch Logo</div>
+    <React.Fragment>
+      <div>TicketCrunch Logo</div>
+      <p style={{ fontSize: "30px", padding: '5rem' }}>This is just some description text to make up the numbers!</p>
+    </React.Fragment>
   );
 }
